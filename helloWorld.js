e7f9e63c80cd88d804180ea62219228b06abee1a
pushfirst <shakti.srivastava0509@gmail.com>
2025-10-18 18:33:09 +0530

    js basics warmup for ds

---
+++ helloWorld.js
@@ -0,0 +1,25 @@
+console.log('Hello World');
+console.log(7);
+console.log(true);
+console.log('Shakti 3');
+const a = 10;
+const b = 20;
+console.log('Number === String', a == '10');
+console.log('Sum', a+b);
+console.log('Mul', a*b);
+console.log('Diff', a-b);
+
+let x = 10;
+console.log('Const === Let', a === x);
+
+let firstName = 'Shakti';
+let lastName = 'Srivastava';
+
+let fullName = firstName+' '+lastName;
+console.log('Full Name', fullName);
+
+console.log('Number === String', fullName+a);
+
+let arr = [1,2,3, 'A', [5,6,7], {a:1, b:2}];
+console.log(arr);
+console.log(arr[5]);
